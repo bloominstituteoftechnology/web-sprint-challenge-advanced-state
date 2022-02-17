7c059b59197235136668d38829fee5a0595e3045
Ladrillo <gabriel@cabrejas.net>
2022-02-17 12:45:32 +0100

    comments

--- frontend/components/Quiz.js
+++ frontend/components/Quiz.js
@@ -4,6 +4,7 @@ export default function Quiz(props) {
   return (
     <div id="wrapper">
       {
+        // quiz already in state? Let's use that, otherwise render "Loading next quiz..."
         true ? (
           <>
             <h2>What is a closure?</h2>
