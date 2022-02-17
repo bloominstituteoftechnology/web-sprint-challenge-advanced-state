0e694ad51386ec0656b8d9980283324dd46fe5d7
Ladrillo <gabriel@cabrejas.net>
2022-02-17 14:41:38 +0100

    comments

--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -15,11 +15,25 @@ export function resetForm() { }
 
 // ❗ async action creators
 export function fetchQuiz() {
-  return function (dispatch) { }
+  return function (dispatch) {
+    // First, reset the quiz state so the "Loading next quiz..." message can display
+    // On successful GET:
+    // - Load the quiz into the quiz state
+  }
 }
 export function postAnswer() {
-  return function (dispatch) { }
+  return function (dispatch) {
+    // On successful POST:
+    // - Reset the selected answer state
+    // - Set the message from the server in the appropriate state
+    // - Dispatch the fetching of the next quiz
+  }
 }
 export function postQuiz() {
-  return function (dispatch) { }
+  return function (dispatch) {
+    // On successful POST:
+    // - Set a message in state
+    // - Reset the form
+  }
 }
+// On promise rejections, use log statements or breakpoints, and put an appropriate erro message in state.
