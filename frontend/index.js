d02ad7d59e5c6d79db38a4f87efd89ddcff56dff
Ladrillo <gabriel@cabrejas.net>
2022-04-02 11:31:58 +0200

    "upgrade deps & lockfile"

--- frontend/index.js
+++ frontend/index.js
@@ -2,9 +2,12 @@
 // 👉 DO NOT CHANGE THIS FILE 👈
 // 👉 DO NOT CHANGE THIS FILE 👈
 import React from 'react'
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './components/App'
 import './styles/reset.css'
 import './styles/styles.css'
 
-render(<App />, document.getElementById('root'))
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+root.render(<App />)
